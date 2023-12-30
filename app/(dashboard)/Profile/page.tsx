268dee711a404e22da3bc76c86664f0b903c178c
michael-172 <michaelmicheal72@gmail.com>
2023-12-30 14:37:43 +0200

    removed unused socket connection

--- app/(dashboard)/Profile/page.tsx
+++ app/(dashboard)/Profile/page.tsx
@@ -3,10 +3,6 @@ import { getOne } from "@/services/service";
 import React from "react";
 
 const Page = async () => {
-  const socket = await new WebSocket("ws://localhost:8080?user_id=5000");
-  // socket.onopen = () => {
-  //   console.log("connected");
-  // };
   return <ProfileMain />;
 };
 
