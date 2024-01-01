0a7eb36c46e1789b900eb0a78ba202a616c055d4
michael-172 <michaelmicheal72@gmail.com>
2024-01-01 18:12:59 +0200

    Created Notifications Module and Fixed some bugs

--- app/(auth)/FinalLayout.tsx
+++ app/(auth)/FinalLayout.tsx
@@ -31,8 +31,8 @@ const FinalLayout = ({ children }: { children: React.ReactNode }) => {
       }}
     >
       <Head>
-        <Link rel="preconnect" href="https://fonts.gstatic.com" />
-        <Link
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap"
           rel="stylesheet"
         />
